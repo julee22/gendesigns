8dfdd73308ee52745d67409e443f5dbdf14428d6
gen <genice6776@hotmail.com>
2023-08-18 20:01:36 -0400

    added richmedia project to carousel version
    fixed merge conflicts

--- js/main-revised.js
+++ js/main-revised.js
@@ -40,79 +40,34 @@ function changePageLinkColor(projectId) {
 
 function changeBarColor(projectId) {
 	var project = projectList.find(project => project.name == projectId);
-	console.log(projectId,"project name");
 	barColor = project.bgColorHex;
 	$("#background-bar").css("background-color",barColor);
 	// also change menu bar color
 	$("#navbar").css("background-color",barColor+"F7");
 }
 
-
-// Main function to show project
-function showProject() {
+function generateCarouselIndicators(numberOfSlides, targetCarousel) {
+	var indicatorWrapper = document.querySelector(".carousel-indicators");
+	console.log("numberOfSlides",numberOfSlides);
 	
-	var activeProj = $("#projectList").children(".project");
+	for (var i = 0; i < numberOfSlides; i++) {
+		var newIndicator = document.createElement('li');
+		newIndicator.setAttribute("data-target", targetCarousel);
+		newIndicator.setAttribute("data-slide-to", i);
 
-	// If mobile
-	const isMobile = /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-	
-	// Changing parameters depending on header height
-	var headerHeight;
-	const headerOffset = $("header")[0].getBoundingClientRect();
-	headerHeight = headerOffset.bottom-25;
-
-	if (window.innerWidth >=768) {
-		$("#projectList").css("margin-top",headerHeight);
-	} 
-
-	window.addEventListener('resize', (event) => {
-		if (window.innerWidth >=768) {
-			//Get height of header
-			// const headerOffset = $("header")[0].getBoundingClientRect();
-			// headerHeight = headerOffset.bottom-25;
-			console.log("header height is "+  headerHeight);
-			$("#projectList").css("margin-top",headerHeight);
-		}	else {
-			$("#projectList").css("margin-top","0");
+		if (i == 0) {
+			newIndicator.classList.add("active");
 		}
-	}, true);
-
-	// shows all projects immediately if mobile
-	if (isMobile) {
-		for (var i = 0; i < projectList.length; i++) {
-			$(activeProj[i]).css("opacity","1");
-		}
-	// } else {
-	// 	$(activeProj[0]).css("opacity","1");
+		indicatorWrapper.appendChild(newIndicator);
 	}
 
-	// Checks if project is in view and decides fades in/out
-	for (var i = 0; i < projectList.length; i++){
-		var selectedProject = activeProj[i];
-		var projectOffset = selectedProject.getBoundingClientRect();
-		var top = projectOffset.top;
-		var height = projectOffset.bottom;
-
-
-		if (top < headerHeight /2 - 25) {
-			if (!isMobile && window.innerWidth >=768) {
-				$(activeProj[i]).removeAttr("style");
-				console.log("hide "+  i);
-			}
-		} else if ((top <= window.innerHeight && top > -50) || height > 100) {
-			changeBarColor(projectList[i].name);
-			if (!isMobile) {
-				$(activeProj[i]).css("opacity","1");
-				console.log("show "+  i);
-			}
-			break;
-		}
-	}
 }
-
 
 //JQUERY
 $(document).ready(function() {
+
+	// If mobile
+	const isMobile = /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 	const listOfTitle = document.getElementsByClassName("main-title");
 	var mainTitle;
@@ -126,11 +81,9 @@ $(document).ready(function() {
 		console.log(mainTitle.id);
 	}
 
-	// Sets project background and shows image
-	showProject();
 
 	//Set initial bar color
-	changeBarColor("default");
+	changeBarColor("rich-media");
 
 
 	// Changing parameters depending on header
@@ -178,10 +131,4 @@ $(document).ready(function() {
 	// Generates carousel indicators automatically
 	const listOfSlides = document.querySelectorAll(".carousel-item");
 	generateCarouselIndicators(listOfSlides.length,"#projectList");
-});
-
-
-// Event changing the bar color and image
-$(document).scroll(function() {
-	showProject();
 });
